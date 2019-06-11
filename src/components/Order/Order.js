9185d4eeedd9a518d6365ffff67a414cf0b0ee5b
Saikumar <saikumarkunchaa@gmail.com>
2019-06-11 11:19:09 +0530

    redux

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import classes from './Order.css';
 const Order = (props) => {
+    const Ingredients = [];
+    for (let IngredientName in props.ingredients) {
+        Ingredients.push({
+            name:IngredientName,
+            amount: props.ingredients[IngredientName]
+        });
+    }
+    const ingredientsOuput = Ingredients.map(ig => {
+        return <span key = {ig.name} style = {{
+            textTransform:'capitalize',
+            display: 'inline-block',
+            margin:'0 8px',
+            border:'1px solid #ccc',
+            padding:'5px'
+        }}>{ig.name} ({ig.amount})</span>;
+    })                  
     return (
         <div className = {classes.Order}> 
-            <p> Ingredients Order : Total cost is 7USD</p>  
+            {ingredientsOuput}
+            <p>Price: <strong>USD {props.price}</strong></p> 
         </div>
     );
 };
